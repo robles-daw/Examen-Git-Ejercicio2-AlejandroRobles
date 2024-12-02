bb826341231a5e2927426695628c011027ad3e20
Alejandro Robles <24.daw.alejandrorobles@funadacionmontessori.com>
2024-12-02 19:55:52 +0100

    Proyecto

--- Proyecto1/src/area/Area.java
+++ Proyecto1/src/area/Area.java
@@ -7,9 +7,9 @@ public class Area {
 	public static void main(String[] args) {
 		Scanner input=new Scanner (System.in);
 		
-		System.out.println("Dame un radio");
-		double radio=input.nextDouble();
-		System.out.println((Math.PI*Math.pow(radio, 2)));
+		System.out.println("Dame un lado");
+		double lado=input.nextDouble();
+		System.out.println((Math.pow(lado, 2)));
 		
 		input.close();
 
